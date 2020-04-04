7a9457728ef04dbc65a12232f15ef8aed804437a
huangchao1234567 <863080579@qq.com>
2020-04-04 16:57:28 +0800

    饿了么插件引入 并写一个测试样式

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Select } from 'element-ui';
+
+import Element from 'element-ui';
+
+
+Vue.use(ElementUI);
+//引入Button 和 Select，插件
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
 Vue.config.productionTip = false
 
+
 new Vue({
+  el: '#app',
   router,
   render: h => h(App)
 }).$mount('#app')
